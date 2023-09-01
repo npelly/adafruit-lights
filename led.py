9eecd5c72e5c4ede7151f07372769ec18491a29c
Nick Pelly <npelly@gmail.com>
2023-09-01 09:37:27 -0700

    [WIP] Simplify processing logic, add rainbow mode and auto-power off.
    Not yet working: out of memory.

--- led.py
+++ led.py
@@ -1,73 +1,52 @@
 # Nick Pelly August 19, 2023
 
-import adafruit_irremote
-import board
-import neopixel
-import pulseio
+from adafruit_irremote import GenericDecode
+from adafruit_irremote import NECRepeatIRMessage
+from adafruit_irremote import FailedToDecode
+from adafruit_irremote import IRDecodeException
+from board import D10
+from board import D5
+from board import NeoPixel as BuiltinNeoPixel
+from neopixel  import NeoPixel
+from pulseio import PulseIn
 import time
 import gc
 
-
 # Board Constants
-IR_INPUT_PIN = board.D10
-LED_OUTPUT_PIN = board.D5
-LED_COUNT = 20
+IR_INPUT_PIN = D10
+LED_OUTPUT_PIN = D5
+LED_COUNT = 16
 
-def action_off():
-    global power_on
-    
-    power_on = False
-    fill_all_leds((0, 0, 0))
-
-def action_on():
-    global power_on
-
-    power_on = True
-    fill_all_leds(selected_color)
-
-def action_brightness_up():
-    global brightness
-    global pixels
-
-    brightness = brightness + 0.05
-    if brightness > 0.4:
-        brightness = 0.4
-    pixels.brightness = brightness
-
-def action_brightness_down():
-    global brightness
-    global pixels
-
-    brightness = brightness - 0.05
-    if brightness <= 0:
-        brightness = 0.05
-    pixels.brightness = brightness
-
-def action_flash():
-    global flash_active
-    global fade_active
-    
-    flash_active = not flash_active
-    fade_active = False
-
-
-def action_fade():
-    global fade_active
-    global flash_active
-
-    fade_active = not fade_active
-    flash_active = False
-
+# Defaults and Settings
+AUTO_POWER_OFF_DURATION = 30 * 60  # seconds
 DEFAULT_COLOR = (252, 3, 194)  # Lucy's Pink
 DEFAULT_BRIGHTNESS = 0.2
+FLASH_TIME_ON = 0.2
+FLASH_TIME_OFF = 0.1
+FADE_STEP_DURATION = 0.05
+RAINBOW_STEP_DURATION = 0.03
 
+# MODE constants
+OFF= 0
+ON = 1
+BRIGHTNESS_UP = 2
+BRIGHTNESS_DOWN = 3
+FLASH = 4
+STROBE = 5
+FADE = 6
+SMOOTH = 7
 
+# IR Remote and Color Constants
 IR_CODE_TABLE = {
     # Generic IR remote, 5x purchased on amazon August 2023
-    (255, 8, 191, 64):  (None, action_off),        # OFF (Black)
-    (255, 8, 63, 192):  (None, action_on),     # ON (White)
-    (255, 8, 255, 0):   (None, action_brightness_up),   # BRIGHTNESS_UP
-    (255, 8, 127, 128): (None, action_brightness_down), # BRIGHTNESS_DOWN
+    (255, 8, 191, 64):  (None, OFF),             # OFF
+    (255, 8, 63, 192):  (None, ON),              # ON
+    (255, 8, 255, 0):   (None, BRIGHTNESS_UP),   # BRIGHTNESS_UP
+    (255, 8, 127, 128): (None, BRIGHTNESS_DOWN), # BRIGHTNESS_DOWN
+    (255, 8, 47, 208):  (None, FLASH),           # FLASH
+    (255, 8, 15, 240):  (None, STROBE),          # STROBE
+    (255, 8, 55, 200):  (None, FADE),            # FADE
+    (255, 8, 23, 232):  (None, SMOOTH),          # SMOOTH
     (255, 8, 223, 32):  ((255, 0, 0), None),            # RED
     (255, 8, 239, 16):  ((252, 94, 3), None),           # RED_2: Lucy's Orange
     (255, 8, 207, 48):  ((255, 69, 0), None),           # RED_3
@@ -84,22 +63,27 @@ IR_CODE_TABLE = {
     (255, 8, 183, 72):  ((252, 3, 215), None),          # BLUE_4: Lucy's Purple
     (255, 8, 151, 104): ((252, 3, 194), None),          # BLUE_5: Lucy's Pink
     (255, 8, 31, 224):  ((255, 255, 255), None),        # WHITE
-    (255, 8, 47, 208):  (None, action_flash),           # FLASH
-    (255, 8, 15, 240):  (None, None),          # STROBE
-    (255, 8, 55, 200):  (None, action_fade),            # FADE
-    (255, 8, 23, 232):  (None, None)           # SMOOTH
 }
 
-
-print(time.monotonic(), "Starting LED controller...")
-
-builtin_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
-pixels = neopixel.NeoPixel(LED_OUTPUT_PIN, LED_COUNT, brightness=DEFAULT_BRIGHTNESS)
-ir_pulse_decoder = adafruit_irremote.GenericDecode()
-
+# State
+power_on = True
+power_off_time = time.monotonic() + AUTO_POWER_OFF_DURATION
+selected_mode = ON
 selected_color = DEFAULT_COLOR
-brightness = DEFAULT_BRIGHTNESS
-pixels.brightness = brightness
+selected_brightness = DEFAULT_BRIGHTNESS
+next_timestamp = 0
+flash_state_on = False
+rainbow_colorwheel = 0
+rainbow_led_index = 0
+
+# Start execution...
+builtin_pixel = NeoPixel(BuiltinNeoPixel, 1)
+pixels = NeoPixel(LED_OUTPUT_PIN, LED_COUNT, brightness=DEFAULT_BRIGHTNESS)
+ir_pulse_decoder = GenericDecode()
+
+gc.collect()
+print("Free:", gc.mem_free())
+print("Allocated:", gc.mem_alloc())
 
 def fill_all_leds(color):
     builtin_pixel.fill(color)
@@ -108,101 +92,158 @@ def fill_all_leds(color):
 def increment_or_skip_timestamp(timestamp, increment):
     timestamp += increment
     if timestamp < time.monotonic():
+        print(".")
         # skip ahead
         timestamp = time.monotonic() + increment
     return timestamp
 
-# --- Power ---
-power_on = True
+def colorwheel(pos):
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (int(255 - pos * 3), int(pos * 3), 0)
+    elif pos < 170:
+        pos -= 85
+        return (0, int(255 - pos * 3), int(pos * 3))
+    else:
+        pos -= 170
+        return (int(pos * 3), 0, int(255 - pos * 3))
 
-# --- Flash ---
-FLASH_TIME_ON = 0.2
-FLASH_TIME_OFF = 0.1
-flash_active = False
-flash_state_on = False
-flash_next_timestamp = 0
+def select_color(color):
+    global selected_color
+    selected_color = color
+    fill_all_leds(color)
 
-def maybe_run_flash():
-    global flash_next_timestamp
-    global flash_state_on
+def handle_button(button):
+    global power_on
+    global selected_mode
+    global selected_brightness
+    global pixels
 
-    if not flash_active:
-        return
-    timestamp = time.monotonic()
-    if timestamp > flash_next_timestamp:
-        flash_state_on = not flash_state_on
-        if flash_state_on:
-            fill_all_leds(selected_color)
-            flash_next_timestamp = increment_or_skip_timestamp(flash_next_timestamp, FLASH_TIME_ON)
+    if button == OFF:
+        power_on = False
+        fill_all_leds((0, 0, 0))
+    elif button == ON:
+        power_on = True
+        pixels.brightness = selected_brightness
+        fill_all_leds(selected_color)    
+    elif button == BRIGHTNESS_UP:
+        selected_brightness += 0.05
+        if selected_brightness > 0.4:
+            selected_brightness = 0.4
+        pixels.brightness = selected_brightness
+    elif button == BRIGHTNESS_DOWN:
+        selected_brightness -= 0.05
+        if selected_brightness <= 0.0:
+            selected_brightness = 0.05
+        pixels.brightness = selected_brightness
+    elif button == FLASH or button == FADE or button == SMOOTH or button == STROBE:
+        if selected_mode == button:
+            selected_mode = ON
+            if power_on:
+                pixels.brightness = selected_brightness
+                fill_all_leds(selected_color)
         else:
-            fill_all_leds((0, 0, 0))
-            flash_next_timestamp = increment_or_skip_timestamp(flash_next_timestamp, FLASH_TIME_OFF)
+            selected_mode = button
 
-# --- Fade ---
-FADE_STEP_DURATION = 0.05
-fade_active = False
-fade_next_timestamp = 0
+def run_mode_task():
+    global next_timestamp
+    global power_on
 
-
-def maybe_run_fade():
-    global fade_next_timestamp
-
-    if not fade_active:
-        return
-    timestamp = time.monotonic()
-    if timestamp > fade_next_timestamp:
-        pixels.brightness -= 0.02
-        if pixels.brightness <= 0:
-            pixels.brightness = 0.4
-
-        fade_next_timestamp = increment_or_skip_timestamp(fade_next_timestamp, FADE_STEP_DURATION)
-
-def run_tasks(ir_pulse_in):
-    run_ir_task(ir_pulse_in)
     if not power_on:
         return
-    maybe_run_flash()
-    maybe_run_fade()
-    #maybe_run_rainbow()
+    
+    timestamp = time.monotonic()
 
+    if timestamp > power_off_time:
 
+        power_on = False
+        fill_all_leds((0, 0, 0))
+        return
+
+    if selected_mode == ON:
+        return
+    
+    if timestamp > next_timestamp:
+        if selected_mode == FLASH:
+            global flash_state_on
+            flash_state_on = not flash_state_on
+            if flash_state_on:
+                fill_all_leds(selected_color)
+                next_timestamp = increment_or_skip_timestamp(next_timestamp, FLASH_TIME_ON)
+            else:
+                fill_all_leds((0, 0, 0))
+                next_timestamp = increment_or_skip_timestamp(next_timestamp, FLASH_TIME_OFF)
+        elif selected_mode == FADE:
+            global pixels
+            pixels.brightness -= 0.02
+            if pixels.brightness <= 0:
+                pixels.brightness = 0.4
+            next_timestamp = increment_or_skip_timestamp(next_timestamp, FADE_STEP_DURATION)
+        elif selected_mode == SMOOTH:
+            global rainbow_colorwheel
+            rainbow_colorwheel += 1
+            rainbow_colorwheel %= 256
+            fill_all_leds(colorwheel(rainbow_colorwheel))
+            next_timestamp = increment_or_skip_timestamp(next_timestamp, RAINBOW_STEP_DURATION)
+        elif selected_mode == STROBE:
+            global rainbow_colorwheel
+            global rainbow_led_index
+
+            pixel_colorwheel = (rainbow_colorwheel + rainbow_led_index * 256 // LED_COUNT) % 256
+            pixels[rainbow_led_index] = colorwheel(pixel_colorwheel)
+            rainbow_colorwheel += 1
+            rainbow_colorwheel %= 256
+            rainbow_led_index += 1
+            rainbow_led_index %= LED_COUNT
+
+            next_timestamp = increment_or_skip_timestamp(next_timestamp, 0.02)
+        
 def run_ir_task(ir_pulse_in):
     global selected_color
-    global brightness
-    global power_on
-    
-    pulses = ir_pulse_decoder.read_pulses(ir_pulse_in, blocking=False)
+    global power_off_time
+
+    pulses = ir_pulse_decoder.read_pulses(ir_pulse_in, blocking=False, max_pulse=100)
+    gc.collect()
+
     if not pulses:
         return
     try:
         code = ir_pulse_decoder.decode_bits(pulses)
+
         print("Received IR code:", code)
-        (color, action) = IR_CODE_TABLE[code]
-        print("...color:", color, "action:", action)
+        (color, button) = IR_CODE_TABLE[code]
+        print("...color:", color, "button:", button)
+
+        power_off_time = time.monotonic() + AUTO_POWER_OFF_DURATION  # Extend deadline
 
         if color:
-            selected_color = color
-            fill_all_leds(color)
-        if action:
-            action()
-    except adafruit_irremote.IRNECRepeatException:  # unusual short code!
+            select_color(color)
+        if button is not None:
+            handle_button(button)
+        print("Free:", gc.mem_free())
+        print("Allocated:", gc.mem_alloc())
+    except NECRepeatIRMessage:  # unusual short code!
         print("NEC repeat!")
-    except adafruit_irremote.IRDecodeException as e:     # failed to decode
+    except IRDecodeException as e:     # failed to decode
         print("Failed to decode: ", e.args)
-    except adafruit_irremote.FailedToDecode:
+    except FailedToDecode:
         print("Failed")
     except KeyError:
         print("...unknown code")
     ir_pulse_in.clear()
-
-with pulseio.PulseIn(IR_INPUT_PIN, maxlen=300, idle_state=True) as ir_pulse_in:
     gc.collect()
     print("Free:", gc.mem_free())
     print("Allocated:", gc.mem_alloc())
-    if power_on:
-        action_on()
+
+with PulseIn(IR_INPUT_PIN, maxlen=50, idle_state=True) as ir_pulse_in:
+    gc.collect()
+    print("Free:", gc.mem_free())
+    print("Allocated:", gc.mem_alloc())
+    
+    fill_all_leds(selected_color)
+
     while True:
-        timestamp = time.monotonic()
-
-        run_tasks(ir_pulse_in)
-
+        run_ir_task(ir_pulse_in)
+        if power_on:
+            run_mode_task()
